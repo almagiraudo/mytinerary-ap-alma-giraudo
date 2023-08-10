5a8a6a69e1227372e87ef64e5fc0e791d6962fc4
Alma <almagdeveloper@gmail.com>
2023-08-10 18:05:37 -0300

    navegacion a cities page

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -1,9 +1,13 @@
+import NavBar from "../components/NavBar";
 
 
 export default function Cities() {
   return (
-    <div className="text-black">
+    <>
+    <NavBar/>
+    <div className="text-black pt-[100px] ">
        <p>Page Cities under construction</p>
     </div>
+    </>
   )
 }
