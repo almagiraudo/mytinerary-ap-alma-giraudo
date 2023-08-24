2fd82a620fd474854200c2716206a7c46b2d432e
Alma <100444289+almagiraudo@users.noreply.github.com>
2023-08-24 13:55:10 -0300

    se agregó el formulario de signUp (#10)

--- src/layouts/NavLayout.jsx
+++ src/layouts/NavLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import { Outlet } from 'react-router-dom'
 
-export default function NavLayout({children}) {
+export default function NavLayout() {
   return (
 <div> 
     <NavBar/> 
-    {children}    
+    <Outlet/>   
     </div>
   )
 }
